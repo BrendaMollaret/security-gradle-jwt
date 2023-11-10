ab7df6a7c826b51bc8619ce16ddb6c04da7f1bf9
Brenda Mollaret <mollaretbrenda@gmail.com>
2023-11-10 20:23:38 -0300

    Funcionando

---
+++ src/main/java/com/bmollaret/security_gradle_jwt/Config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.bmollaret.security_gradle_jwt.Config;
+
+
+
+import com.bmollaret.security_gradle_jwt.JWT.JwtAuthenticationFilter;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+    {
+        return http
+                .csrf(csrf ->
+                        csrf
+                        .disable())
+                .authorizeHttpRequests(authRequest ->
+                                authRequest
+                                        .requestMatchers("/auth/**").permitAll()
+                                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                        .requestMatchers("/api/v1/demoAdmin/**").hasAuthority("ADMIN")
+                                        .requestMatchers("/api/v1/demoUser/**").hasAuthority("USER")
+                )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                //Para que h2 si pueda acceder
+                .sessionManagement(sessionManager->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+
+    }
+
+}
+
